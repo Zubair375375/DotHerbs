cf5a9bedf5dd44e4391dac38f5c0a159b3f6243f
Muhammad Zubair <muhammadzubair375375@gmail.com>
2026-04-28 01:38:14 +0500

    line removed again

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 // Force dotenv to load the correct env file from backend directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 dotenv.config({
   path: path.join(
     __dirname,
